9f76304bc5b85c3ab10625fd2b345c7aa08c938a
MOSTAFAEZZAT <mostafaezzat318@gmail.com>
2025-06-13 10:50:32 -0400

    adding more tests

--- tests/pokemon.spec.js
+++ tests/pokemon.spec.js
@@ -5,6 +5,7 @@ test.describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('http://localhost:5000') // <- Put your actual app URL here
     await expect(page.getByText('Ivysaur')).toBeVisible()
+    await expect(page.getByText('Ninetales')).toBeVisible()
     await expect(
       page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')
     ).toBeVisible()
